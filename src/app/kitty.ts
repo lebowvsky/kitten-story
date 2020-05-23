39f795a8db4468b9e7111f4579bfc44148640fc4
Brice Le Gallo <lebowvsky@gmail.com>
2020-05-23 16:26:33 +0200

    clean

--- src/app/kitty.ts
+++ src/app/kitty.ts
@@ -11,35 +11,6 @@ export class Kitty {
   picture: string;
 }
 
-export let kitten: Kitty[] = [
-  {
-    name: 'Jean',
-    breed: 'Persan',
-    birthdate: '13/03/2020',
-    picture:
-      'https://www.wanimo.com/veterinaire/wp-content/uploads/2019/12/prenoms-chaton-r-2020@2x.jpg',
-  },
-  {
-    name: 'Phil',
-    breed: 'Persan',
-    birthdate: '13/03/2020',
-    picture:
-      'https://www.santevet.com/upload/admin/images/article/chaton%20adoption.jpg',
-  },
-  {
-    name: 'Lou',
-    breed: 'Persan',
-    birthdate: '13/03/2020',
-    picture:
-      'https://www.wanimo.com/veterinaire/wp-content/uploads/2019/12/prenoms-chaton-r-2020@2x.jpg',
-  },
-  {
-    name: 'Xav',
-    breed: 'Persan',
-    birthdate: '13/03/2020',
-    picture:
-      'https://www.wanimo.com/veterinaire/wp-content/uploads/2019/12/prenoms-chaton-r-2020@2x.jpg',
-  }
-];
+export let kitten: Kitty[] = [];
 
 export let adoptedKitten: Kitty[] = [];
